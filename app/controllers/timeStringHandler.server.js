0c32c7476970fee15f3373ada72fe43bd2103c98
heldderarbeit <heldderarbeit@users.noreply.github.com>
2016-11-24 13:19:44 +0100

    Update timeStringHandler.server.js

--- app/controllers/timeStringHandler.server.js
+++ app/controllers/timeStringHandler.server.js
@@ -6,27 +6,27 @@ var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
 
 function TimeStringHandler () {
 	
-	this.parseTime = function (req, res) {
+    this.parseTime = function (req, res) {
 	    
         var timestr = req.params.timestr;
         
         // assume it's not a timestring
         var natural = null;
-		var unix = new Date(timestr).getTime() / 1000;
+	var unix = new Date(timestr).getTime() / 1000;
 		
-		// is a natural language date
-		if (unix) natural = timestr; 
+	    // is a natural language date
+	    if (unix) natural = timestr; 
 		
-		if (!isNaN(timestr)) {
+	    if (!isNaN(timestr)) {
 		
-		    // is a unix timestamp
-		    unix = Math.floor(timestr);
-		    var date = new Date(timestr*1000);
+	        // is a unix timestamp
+		unix = Math.floor(timestr);
+		var date = new Date(timestr*1000);
 	        natural = monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();		
 	    }
 	    
-	    res.send({ 'unix': unix, 'natural': natural });
-	};
+        res.send({ 'unix': unix, 'natural': natural });
+    };
 }
 
 module.exports = TimeStringHandler;
