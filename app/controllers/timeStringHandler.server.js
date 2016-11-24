40146ea989085d7e657cc931b6eba94255ca9728
heldderarbeit <heldderarbeit@users.noreply.github.com>
2016-11-24 19:56:21 +0100

    Update timeStringHandler.server.js

--- app/controllers/timeStringHandler.server.js
+++ app/controllers/timeStringHandler.server.js
@@ -14,16 +14,16 @@ function TimeStringHandler () {
         var natural = null;
 	var unix = new Date(timestr).getTime() / 1000;
 		
-	    // is a natural language date
-	    if (unix) natural = timestr; 
+	// is a natural language date
+	if (unix) natural = timestr; 
 		
-	    if (!isNaN(timestr)) {
+	if (!isNaN(timestr)) {
 		
-	        // is a unix timestamp
-		unix = Math.floor(timestr);
-		var date = new Date(timestr*1000);
-	        natural = monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();		
-	    }
+	    // is a unix timestamp
+	    unix = Math.floor(timestr);
+	    let date = new Date(timestr*1000);
+	    natural = monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();		
+	}
 	    
         res.send({ 'unix': unix, 'natural': natural });
     };
